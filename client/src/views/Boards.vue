88a399a771a04a34e0fb842a237411adc19423e8
John Bonde <jjbonde1@gmail.com>
2020-01-14 11:03:41 -0700

    landing page layout complete

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="boards">
-    WELCOME TO THE BOARDS!!!
+    <form v-if="loginForm" @submit.prevent="loginUser">
+      <input type="email" v-model="creds.email" placeholder="email" />
+      <input type="password" v-model="creds.password" placeholder="password" />
+      <button class="btn btn-success" type="submit">Login</button>
+    </form>
+    <form v-else @submit.prevent="register">
+      <input type="text" v-model="newUser.name" placeholder="name" />
+      <input type="email" v-model="newUser.email" placeholder="email" />
+      <input type="password" v-model="newUser.password" placeholder="password" />
+      <button class="btn btn-warning" type="submit">Create Account</button>
+    </form>
+    <div class="action" @click="loginForm = !loginForm">
+      <p v-if="loginForm">No account? Click here to Register</p>
+      <p v-else>Already have an account? Click here to Login</p>
+    </div>WELCOME TO THE BOARDS!!!
     <form @submit.prevent="addBoard">
-      <input type="text" placeholder="title" v-model="newBoard.title" required>
-      <input type="text" placeholder="description" v-model="newBoard.description">
+      <input type="text" placeholder="title" v-model="newBoard.title" required />
+      <input type="text" placeholder="description" v-model="newBoard.description" />
       <button type="submit">Create Board</button>
     </form>
     <div v-for="board in boards" :key="board._id">
@@ -13,29 +27,29 @@
 </template>
 
 <script>
-  export default {
-    name: "boards",
-    mounted() {
-      this.$store.dispatch("getBoards");
-    },
-    data() {
-      return {
-        newBoard: {
-          title: "",
-          description: ""
-        }
-      };
-    },
-    computed: {
-      boards() {
-        return this.$store.state.boards;
+export default {
+  name: "boards",
+  mounted() {
+    this.$store.dispatch("getBoards");
+  },
+  data() {
+    return {
+      newBoard: {
+        title: "",
+        description: ""
       }
-    },
-    methods: {
-      addBoard() {
-        this.$store.dispatch("addBoard", this.newBoard);
-        this.newBoard = { title: "", description: "" };
-      }
+    };
+  },
+  computed: {
+    boards() {
+      return this.$store.state.boards;
     }
-  };
+  },
+  methods: {
+    addBoard() {
+      this.$store.dispatch("addBoard", this.newBoard);
+      this.newBoard = { title: "", description: "" };
+    }
+  }
+};
 </script>
---
+++ client/src/views/Landing.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="landing container-fluid">
+    <div class="row house">
+      <div class="col">
+        <div class="arrow-up"></div>
+      </div>
+      <div class="col">
+        <div class="body"></div>
+      </div>
+      <div class="col d-flex login">
+        <button class="btn btn-primary">Login</button>
+        <h3>|</h3>
+        <button class="btn btn-primary">Register</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from "@/router/index.js";
+export default {
+  name: "landing",
+  data() {
+    return {
+      loginForm: true,
+      creds: {
+        email: "",
+        password: ""
+      },
+      newUser: {
+        email: "",
+        password: "",
+        name: ""
+      }
+    };
+  },
+  beforeCreate() {
+    if (this.$store.state.user._id) {
+      this.$router.push({ name: "boards" });
+    }
+  },
+  methods: {
+    register() {
+      this.$store.dispatch("register", this.newUser);
+    },
+    loginUser() {
+      this.$store.dispatch("login", this.creds);
+    }
+  }
+};
+</script>
+
+<style>
+.house {
+  display: flex;
+}
+.arrow-up {
+  margin: auto;
+  margin-top: 75px;
+  width: 0;
+  height: 0;
+  border-left: 150px solid transparent;
+  border-right: 150px solid transparent;
+  border-bottom: 150px solid rgb(109, 54, 54);
+}
+.body {
+  position: relative;
+  margin: auto;
+  width: 0;
+  margin-top: -1px;
+  border: 130px solid rgb(109, 54, 54);
+}
+.action {
+  cursor: pointer;
+}
+.login {
+  margin-top: 30px;
+  justify-content: center;
+}
+.btn-primary {
+  background-color: transparent;
+  color: black;
+  border: none;
+}
+</style>
