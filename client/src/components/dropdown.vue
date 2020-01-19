0ed951600c460192998e551793663e5873ee1a42
renwickp <peter2renwick@gmail.com>
2020-01-18 19:07:06 -0700

    drop down vue

---
+++ client/src/components/dropdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dropdown">
+    <button
+      class="btn btn-secondary dropdown-toggle"
+      type="button"
+      id="dropdownMenuButton"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      Dropdown button
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Do Laundry</a>
+      <a class="dropdown-item" href="#">Mow Lawn</a>
+      <a class="dropdown-item" href="#">Wash Dishes</a>
+      <a class="dropdown-item" href="#">Clean Bathroom</a>
+      <a class="dropdown-item" href="#">Wash Dishes</a>
+      <a class="dropdown-item" href="#">Clean Room</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dropdown",
+  props: ["dropData"]
+};
+</script>
+
+<style></style>
