56b2fff5b1816b86292fbf39194b84ed925595ad
MadisonIngraham <madi.ingraham@gmail.com>
2020-01-31 11:13:37 -0700

    can edit house name

--- server/controllers/HousesController.js
+++ server/controllers/HousesController.js
@@ -1,101 +1,110 @@
-import _housesService from "../services/HousesService.js";
-import express from "express";
-import { Authorize } from "../middleware/authorize.js";
+import _housesService from '../services/HousesService.js'
+import express from 'express'
+import { Authorize } from '../middleware/authorize.js'
 
-import _usersService from "../services/UsersService";
+import _usersService from '../services/UsersService'
 
-import _choresService from "../services/choresService.js";
+import _choresService from '../services/choresService.js'
 
 //PUBLIC
 export default class HousesController {
-  constructor() {
+  constructor () {
     this.router = express
       .Router()
       .use(Authorize.authenticated)
-      .get("/:id", this.getHouseById)
-      .get("/:id/rels", this.getRelsByHouse)
-      .get("/:id/chores", this.getChoresByHouse)
-      .post("", this.createHouse)
-      .post("/:id", this.addRoom)
-      .put("/:id", this.edit)
-      .put("/:id/chores", this.editChore)
-      .delete("/:id", this.delete)
-      .use(this.defaultRoute);
+      .get('/:id', this.getHouseById)
+      .get('/:id/rels', this.getRelsByHouse)
+      .get('/:id/chores', this.getChoresByHouse)
+      .post('', this.createHouse)
+      .post('/:id', this.addRoom)
+      .put('/:id', this.getChoreByHouseAndProfile)
+      .put('/:id/house', this.edit)
+      .put('/:id/chores', this.editChore)
+      .delete('/:id', this.delete)
+      .use(this.defaultRoute)
   }
 
-  defaultRoute(req, res, next) {
-    next({ status: 404, message: "No Such Route" });
+  defaultRoute (req, res, next) {
+    next({ status: 404, message: 'No Such Route' })
   }
 
-  async getHouseById(req, res, next) {
+  async getHouseById (req, res, next) {
     try {
-      let data = await _housesService.getHouseById(req.params.id);
-      return res.send(data);
+      let data = await _housesService.getHouseById(req.params.id)
+      return res.send(data)
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-  async getRelsByHouse(req, res, next) {
+  async getRelsByHouse (req, res, next) {
     try {
-      let data = await _housesService.getRelsByHouse(req.params.id);
-      return res.send(data);
+      let data = await _housesService.getRelsByHouse(req.params.id)
+      return res.send(data)
     } catch (error) {
-      next(error);
-    }
-  }
-
-  async createHouse(req, res, next) {
-    try {
-      let data = await _housesService.createHouse(req.body);
-      return res.status(201).send(data);
-    } catch (error) {
-      next(error);
-    }
-  }
-  async addRoom(req, res, next) {
-    try {
-      let data = await _usersService.getByEmail(req.body);
-      return res.status(201).send(data);
-    } catch (error) {
-      next(error);
+      next(error)
     }
   }
 
-  async edit(req, res, next) {
+  async createHouse (req, res, next) {
+    try {
+      let data = await _housesService.createHouse(req.body)
+      return res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async addRoom (req, res, next) {
+    try {
+      let data = await _usersService.getByEmail(req.body)
+      return res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getChoreByHouseAndProfile (req, res, next) {
     try {
       let data = await _choresService.getChoreByHouseAndProfile(
         req.params.id,
         req.body
-      );
-      return res.send(data);
+      )
+      return res.send(data)
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-  async editChore(req, res, next) {
+  async edit (req, res, next) {
     try {
-      let data = await _choresService.editChore(req.params.id, req.body);
-      return res.send(data);
+      let data = await _housesService.edit(req.params.id, req.body)
+      return res.send(data)
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
-  async delete(req, res, next) {
+  async editChore (req, res, next) {
     try {
-      await _housesService.delete(req.params.id, req.body);
-      return res.send("Successfully deleted");
+      let data = await _choresService.editChore(req.params.id, req.body)
+      return res.send(data)
     } catch (error) {
-      next(error);
+      next(error)
+    }
+  }
+  async delete (req, res, next) {
+    try {
+      await _housesService.delete(req.params.id, req.body)
+      return res.send('Successfully deleted')
+    } catch (error) {
+      next(error)
     }
   }
 
   //CHORES
-  async getChoresByHouse(req, res, next) {
+  async getChoresByHouse (req, res, next) {
     try {
-      let data = await _choresService.getChoresByHouse(req.params.id);
-      return res.send(data);
+      let data = await _choresService.getChoresByHouse(req.params.id)
+      return res.send(data)
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
 }
--- server/services/HousesService.js
+++ server/services/HousesService.js
@@ -1,73 +1,75 @@
-import mongoose from "mongoose";
-import House from "../models/House";
-import Profile from "../models/Profile";
-import ApiError from "../utils/ApiError";
-import HouseRel from "../models/HouseRel";
-import _choresService from "../services/ChoresService";
+import mongoose from 'mongoose'
+import House from '../models/House'
+import Profile from '../models/Profile'
+import ApiError from '../utils/ApiError'
+import HouseRel from '../models/HouseRel'
+import _choresService from '../services/ChoresService'
 
-const _repository = mongoose.model("House", House);
-const _houseRelRepository = mongoose.model("HouseRel", HouseRel);
+const _repository = mongoose.model('House', House)
+const _houseRelRepository = mongoose.model('HouseRel', HouseRel)
 
 class HousesService {
   //#region -- HOUSE REL FUNCTIONS --
-  async createHouseRel(relData) {
-    let data = await _houseRelRepository.create(relData);
-    _choresService.createChore(relData);
-    return data;
+  async createHouseRel (relData) {
+    let data = await _houseRelRepository.create(relData)
+    _choresService.createChore(relData)
+    return data
   }
-  async getRelsByProfile(id) {
+  async getRelsByProfile (id) {
     return await _houseRelRepository
       .find({ profileId: id })
       // returns rel ID
-      .populate("houseId");
+      .populate('houseId')
     // returns rel ID
   }
-  async getRelsByHouse(id) {
+  async getRelsByHouse (id) {
     return await _houseRelRepository
       .find({ houseId: id })
       // returns rel ID
-      .populate("profileId");
+      .populate('profileId')
     // returns rel ID
   }
   //#endregion
 
   //#endregion
-  async createHouse(rawData) {
-    rawData.admins = rawData.creator;
-    let data = await _repository.create(rawData);
+  async createHouse (rawData) {
+    rawData.admins = rawData.creator
+    let data = await _repository.create(rawData)
     let relData = {
       houseId: data._id,
       //@ts-ignore
       profileId: data.creator
-    };
-    await this.createHouseRel(relData);
-    return data;
+    }
+    await this.createHouseRel(relData)
+    return data
   }
 
-  async getHouseById(id) {
-    let data = await _repository.find({ _id: id });
-    return data;
+  async getHouseById (id) {
+    let data = await _repository.find({ _id: id })
+    return data
   }
-  async edit(id, update) {
-    if (update.admins == update.profileId) {
-      let data = await _repository.findOneAndUpdate({ _id: id }, update, {
-        new: true
-      });
-      return data;
-    } else {
-      throw new ApiError("Not admin cannot delete", 400);
+  async edit (id, update) {
+    for (let i = 0; i <= update.admins.length; i++) {
+      const element = update.admins[i]
+      if (element == update.profileId) {
+        let data = await _repository.findOneAndUpdate({ _id: id }, update, {
+          new: true
+        })
+        return data
+      }
     }
+    throw new ApiError('Not admin cannot delete', 400)
   }
-  async delete(id, houseData) {
+  async delete (id, houseData) {
     if (houseData.profileId == houseData.adminId) {
       let data = await _repository.findOneAndRemove({
         _id: id
-      });
+      })
     } else {
-      throw new ApiError("Not admin cannot delete", 400);
+      throw new ApiError('Not admin cannot delete', 400)
     }
   }
 }
 
-const _housesService = new HousesService();
-export default _housesService;
+const _housesService = new HousesService()
+export default _housesService
